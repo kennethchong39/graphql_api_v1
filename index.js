de2764b04c5a54a370a297e028387ba1cffab310
Kenneth Chong <31711744+kennethchong39@users.noreply.github.com>
2020-11-13 07:45:35 -0800

    add start script

--- index.js
+++ index.js
@@ -75,8 +75,6 @@ const resolvers = {
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen(
-  port: process.env.PORT || 4000
-).then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`Server started at ${url}`);
 });
